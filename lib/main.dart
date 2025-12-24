c0452c620fe7072dcd88adf5127ac163c5a2aa34
Debit Bhattarai <bhatd25@uwgb.edu>
2025-12-23 22:46:32 -0600

    Debit completed setup

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('Hello from Himal!'),
+            const Text('Hello from Debit!'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
