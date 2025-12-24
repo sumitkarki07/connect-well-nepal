4cd39d1d8a06af91348a2dafe659007126281daa
himalSubedi0602 <himalsubedi0602@gmail.com>
2025-12-23 22:20:27 -0600

    Himal Completed Setup

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('Hi I am Himal!'),
+            const Text('Hello from Himal!'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
